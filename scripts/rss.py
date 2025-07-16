a6f28b4b779a1fb3168849be3f302b1624d7a94b
Luis Victoria <v@luis.vi>
2025-07-16 16:22:03 -0400

    Create initial emplate for RSS generator

---
+++ scripts/rss.py
@@ -0,0 +1,12 @@
+"""feed.atom generator"""
+# Using https://kevincox.ca/2022/05/06/rss-feed-best-practices/ as a guide
+
+# CONSTANTS
+HOMEPAGE_URL: str = "https://luis.vi"
+AUTHOR_NAME: str = "Luis Victoria"
+
+def main() -> None:
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
