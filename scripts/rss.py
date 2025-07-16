f91d025e73c3fe622d0514606cf3976eaa62b7f4
Luis Victoria <v@luis.vi>
2025-07-16 17:03:26 -0400

    Create template string for `feed.atom`

--- scripts/rss.py
+++ scripts/rss.py
@@ -2,8 +2,30 @@
 # Using https://kevincox.ca/2022/05/06/rss-feed-best-practices/ as a guide
 
 # CONSTANTS
+FEED_NAME: str = "LV"
 HOMEPAGE_URL: str = "https://luis.vi"
 AUTHOR_NAME: str = "Luis Victoria"
+
+TEMPLATE: str = f"""<?xml version="1.0" encoding="UTF-8"?>
+<feed xmlns="http://www.w3.org/2005/Atom">
+	<title>{FEED_NAME}</title>
+	<id>{HOMEPAGE_URL}</id>
+	<link rel="alternate" href="{HOMEPAGE_URL}"/>
+	<link rel="self" href="{HOMEPAGE_URL+"/feed.atom"}"/>
+	<updated>{{LAST_UPDATE_TIME in RFC3339 format}}</updated>
+	<author>
+		<name>{AUTHOR_NAME}</name>
+	</author>
+	<entry>
+		<title>{{ENTRY.TITLE}}</title>
+		<link rel="alternate" type="text/html" href="{{ENTRY.HTML_URL}}"/>
+		<id>{{ENTRY.PERMALINK}}</id>
+		<published>{{ENTRY.FIRST_POST_TIME in RFC3339 format}}</published>
+		<updated>{{ENTRY.LAST_UPDATE_TIME in RFC3339 format}}</updated>
+		<content type="html">{{ENTRY.HTML}}</content>
+	</entry>
+</feed>
+"""
 
 def main() -> None:
     print("Hello world")
